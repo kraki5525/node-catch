2d70e0a1175a6eb60c5699b4b16340f280bc2de3
Richard McCollister <kraki5525@gmail.com>
2015-05-06 23:39:22 -0400

    start converting code to co

--- commands/refresh.js
+++ commands/refresh.js
@@ -2,176 +2,192 @@ var _ = require('lodash'),
     co = require('co'),
     Promise = require('bluebird'),
     fs = require('mz/fs'),
+    feedParser = require('co-feedparser');
 
-    FeedParser = require('feedparser'),
     request = require('request'),
-    //fs = require('fs'),
     path = require('path'),
-    urlParser = require('url'),
-    //async = require('async-q'),
-    //Q = require('q');
-    ;
+    urlParser = require('url');
 
 var configureFunction = function (program, db, config) {
     program
         .command('refresh')
         .description('Download new podcasts.')
         .action(function () {
-            var dbFind = Q.denodeify(db.find.bind(db));
-            var readDirPromise = Q.denodeify(fs.readdir);
-            var mkDirPromise = Q.denodeify(fs.mkdir);
+            var dbFind = Promise.promisify(db.find, db);
 
-            dbFind({})
-            .then(function (feeds) {
-                return _.chain(feeds)
-                        .map(function(feed) {
-                            return makeFeedTask(feed, db, config);
-                        })
-                        .toArray()
-                        .value();
-            })
-            .then(function (feedTasks) {
-                return async.parallelLimit(feedTasks,4);
-            })
-            .then(function (feeds) {
-                return _.chain(feeds)
-                        .each(function (feed) {
-                            if (feed.folder == null || feed.folder == "") 
-                                feed.folder = path.join(config.storageDirectory, feed.title);
-                        })
-                        .toArray()
-                        .value();
-            })
-            .then(function (feeds) {
-                var dirTasks = _.chain(feeds)
-                                .map(function (feed) {
-                                    return makeDirectoryTask(feed);
-                                })
-                                .toArray()
-                                .value();
-                return async.parallelLimit(dirTasks, config.maxCurrency);
-            })
-            .then(function (feeds) {
-                _.each(feeds, function (feed) {
-                    db.update({_id: feed._id}, feed);
-                });
-            })
-            .then(function () {
-                return dbFind({"items.status": "none"});
-            })
-            .then(function (feedsWithFiles) {
-                return _.chain(feedsWithFiles)
-                         .map(function (feed) {
-                            var items = _.where(feed.items, {status: "none"});
-                            return _.map(items, function (item) {
-                               return {feed: feed, file: item}; 
-                            });
-                         })
-                         .toArray()
-                         .flatten()
-                         .value();
-            })
-            .then(function (feedFiles) {
-               return _.chain(feedFiles)
-                        .filter(function (feedFile) {
-                            return feedFile.status != 'done'
-                        })
-                        .map(function (feedFile) {
-                           return makeFileTask(feedFile, db, config);
-                        })
-                        .value();
-            })
-            .then(function (fileTasks) {
-                return async.parallelLimit(fileTasks,config.maxCurrency);
-            })
-            .then(function (items) {
-                console.log(items);
-                console.log('done');    
-            })
-            .catch(function (reason) {
-                console.log("fail: " + reason);       
+            co(function * () {
+                var feeds = yield dbFind({});
+                var feedTasks = _.map(feeds, function(feed) { return makeFeedTask(feed, db, config); });
+                var results = yield feedTasks;
+
+                console.log(results);
             });
+
+//            .then(function (feeds) {
+//                return _.chain(feeds)
+//                        .map(function(feed) {
+//                            return makeFeedTask(feed, db, config);
+//                        })
+//                        .toArray()
+//                        .value();
+//            })
+//            .then(function (feedTasks) {
+//                return async.parallelLimit(feedTasks,4);
+//            })
+//            .then(function (feeds) {
+//                return _.chain(feeds)
+//                        .each(function (feed) {
+//                            if (feed.folder == null || feed.folder == "") 
+//                                feed.folder = path.join(config.storageDirectory, feed.title);
+//                        })
+//                        .toArray()
+//                        .value();
+//            })
+//            .then(function (feeds) {
+//                var dirTasks = _.chain(feeds)
+//                                .map(function (feed) {
+//                                    return makeDirectoryTask(feed);
+//                                })
+//                                .toArray()
+//                                .value();
+//                return async.parallelLimit(dirTasks, config.maxCurrency);
+//            })
+//            .then(function (feeds) {
+//                _.each(feeds, function (feed) {
+//                    db.update({_id: feed._id}, feed);
+//                });
+//            })
+//            .then(function () {
+//                return dbFind({"items.status": "none"});
+//            })
+//            .then(function (feedsWithFiles) {
+//                return _.chain(feedsWithFiles)
+//                         .map(function (feed) {
+//                            var items = _.where(feed.items, {status: "none"});
+//                            return _.map(items, function (item) {
+//                               return {feed: feed, file: item}; 
+//                            });
+//                         })
+//                         .toArray()
+//                         .flatten()
+//                         .value();
+//            })
+//            .then(function (feedFiles) {
+//               return _.chain(feedFiles)
+//                        .filter(function (feedFile) {
+//                            return feedFile.status != 'done'
+//                        })
+//                        .map(function (feedFile) {
+//                           return makeFileTask(feedFile, db, config);
+//                        })
+//                        .value();
+//            })
+//            .then(function (fileTasks) {
+//                return async.parallelLimit(fileTasks,config.maxCurrency);
+//            })
+//            .then(function (items) {
+//                console.log(items);
+//                console.log('done');    
+//            })
+//            .catch(function (reason) {
+//                console.log("fail: " + reason);       
+//            });
         });
 };
 
 function makeFeedTask (feed, config) {
-    return function () {
-        var deferred = Q.defer();
+    return function * () {
+        var parsedFeed = yield feedParser(feed.url);
+        feed.title = parsedFeed.title;
+        feed.description = parsedFeed.description;
+        console.log('===== %s =====', parsedFeed.title);
 
-        request(feed.url)
-        .pipe(new FeedParser())
-        .on('error', function (error) {
-            console.error(error);
-            deferred.resolve();
-        })
-        .on('meta', function (meta) {
-            feed.title = meta.title;
-            feed.description = meta.description;
-            console.log('===== %s =====', meta.title);
-        })
-        .on('readable', function () {
-            var stream = this, 
-                item;
-            while (item = stream.read()) {
-                for (var i = 0; i < item.enclosures.length; i++) {
-                    var feedItem = createFeedItem(item);
-
-                    if (!itemExists(feedItem, feed))
-                        feed.items.push(feedItem);
-                }
-            }
-        })
-        .on('end', function() {
-            deferred.resolve(feed);
-        });
-
-        return deferred.promise;
+        return parsedFeed;
+//        for (var i = 0; i < parsedFeed.articles.length; i++) {
+//            var item = parsedFeed.articles[i];
+//            var feedItem = createFeedItem(item);
+//            if (!itemExists(feedItem, feed)) {
+//                feed.items.push(feedItem);
+//            }
+//        }
     };
 }
+//    return function () {
+//        var deferred = Q.defer();
+//
+//        request(feed.url)
+//        .pipe(new FeedParser())
+//        .on('error', function (error) {
+//            console.error(error);
+//            deferred.resolve();
+//        })
+//        .on('meta', function (meta) {
+//            feed.title = meta.title;
+//            feed.description = meta.description;
+//            console.log('===== %s =====', meta.title);
+//        })
+//        .on('readable', function () {
+//            var stream = this, 
+//                item;
+//            while (item = stream.read()) {
+//                for (var i = 0; i < item.enclosures.length; i++) {
+//                    var feedItem = createFeedItem(item);
+//
+//                    if (!itemExists(feedItem, feed))
+//                        feed.items.push(feedItem);
+//                }
+//            }
+//        })
+//        .on('end', function() {
+//            deferred.resolve(feed);
+//        });
+//
+//        return deferred.promise;
+//    };
 
-function makeFileTask(item, db, config) {
-    return function() {
-        var deferred = Q.defer();
-
-        var url = urlParser.parse(item.file.files[0]);
-        var fileName = url.pathname.split('/').pop();
-
-        request(urlParser.format(url))
-        .on('response', function() { console.log('downloading ' + url.href); })
-        .on('end', function() { 
-            item.file.status = 'done';
-            deferred.resolve(item); 
-        })
-        .pipe(fs.createWriteStream(path.join(item.feed.folder, fileName)));
-
-        return deferred.promise;
-    };
-}
-
-function makeDirectoryTask(feed) {
-    return function() {
-        var deferred = Q.defer();
-
-        fs.readdir(feed.folder, function (err, files) {
-            if (err) {
-                fs.mkdir(feed.folder, function (err) {
-                    if (err) {
-                        console.log(err)
-                        deferred.reject(err);
-                    }
-                    else {
-                        deferred.resolve(feed);
-                    }
-                })
-            }
-            else {
-                deferred.resolve(feed);
-            }
-        });
-
-        return deferred.promise;
-    };
-}
+//function makeFileTask(item, db, config) {
+//    return function() {
+//        var deferred = Q.defer();
+//
+//        var url = urlParser.parse(item.file.files[0]);
+//        var fileName = url.pathname.split('/').pop();
+//
+//        request(urlParser.format(url))
+//        .on('response', function() { console.log('downloading ' + url.href); })
+//        .on('end', function() { 
+//            item.file.status = 'done';
+//            deferred.resolve(item); 
+//        })
+//        .pipe(fs.createWriteStream(path.join(item.feed.folder, fileName)));
+//
+//        return deferred.promise;
+//    };
+//}
+//
+//function makeDirectoryTask(feed) {
+//    return function() {
+//        var deferred = Q.defer();
+//
+//        fs.readdir(feed.folder, function (err, files) {
+//            if (err) {
+//                fs.mkdir(feed.folder, function (err) {
+//                    if (err) {
+//                        console.log(err)
+//                        deferred.reject(err);
+//                    }
+//                    else {
+//                        deferred.resolve(feed);
+//                    }
+//                })
+//            }
+//            else {
+//                deferred.resolve(feed);
+//            }
+//        });
+//
+//        return deferred.promise;
+//    };
+//}
 
 function createFeedItem(item) {
     var feedItem = {};
