78965436565c96a4e9b0a50cc28cf29abe740e5c
Rich McCollister <kraki5525@gmail.com>
2015-05-12 19:35:07 -0400

    Set Name to a default value after add a podcast.

--- commands/add.js
+++ commands/add.js
@@ -3,7 +3,10 @@ var configureFunction = function(program, db, config) {
         .command('add <url>')
         .description('Add a podcast to the list of podcasts subscribed to.')
         .action(function(url) {
-            db.insert({title: "", description: "", url: url, folder: "", items: []});
+            var displayUrl = url.length > 25 
+                            ? url.substr(0,25) + '...'
+                            : url;
+            db.insert({title: 'New Podcast [' + displayUrl + ']', description: "", url: url, folder: "", items: []});
         });
 };
 
